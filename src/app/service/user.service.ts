6924a5f77e7c48ab560f9152768016ade4e72bf0
Hemant <hemantgitac01@gmail.com>
2025-07-03 17:52:55 +0530

    Update User Registration Service

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,6 +1,6 @@
 import { EventEmitter, Injectable, OnInit } from '@angular/core';
 import { ISelletLogin, IUserSignUp } from '../interface/seller-details';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -8,11 +8,14 @@ import { Router } from '@angular/router';
 })
 export class UserService {
   //API_URL = 'https://ecomm-pearl-phi.vercel.app';
-  API_URL = 'http://localhost:3000'; 
+  //API_URL = 'http://localhost:3000'; 
   //API_URL = 'https://api.jsonbin.io/v3/b/6860d43c8960c979a5b37ec2';
+  API_URL  = 'https://api.jsonbin.io/v3/b/6860d43c8960c979a5b37ec2';
+  MASTER_KEY = '$2a$10$d8jUJPX5oYZKLv1ImkiNmur76JC2.RujA/KrrKi1AMUvchJNulZnG';
+  
   invalidUserAuth = new EventEmitter<boolean>(false); 
   constructor(private _http: HttpClient, private router: Router) { }
-  userSignUp(user: IUserSignUp){
+  /*userSignUp(user: IUserSignUp){
     debugger;
     console.log(user);
     this._http.post(`${this.API_URL}/users`, user, {observe: 'response'})
@@ -21,7 +24,26 @@ export class UserService {
       localStorage.setItem('user',JSON.stringify(result.body));
       this.router.navigate(['/']);
     });
-  }
+  }*/
+ //*******************To post data to other server deployed to JSONBIN.io */
+ userSignUp(user: IUserSignUp){
+    debugger;
+    console.log(user);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-Master-Key': this.MASTER_KEY
+    });
+    return this._http.post(`${this.API_URL}`, user, {
+      headers: headers,
+      observe: 'response'
+    }).subscribe((result) => {
+      debugger;
+      console.log('After user registration, result is: ',result);
+      localStorage.setItem('user',JSON.stringify(result.body));
+      this.router.navigate(['/']);
+    })
+ }
+ //*************************post data end */
   userAuthReload(){
     if(localStorage.getItem('user')){
       this.router.navigate(['/']);
